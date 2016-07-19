bf87b9181e336ea5641db36148f2dbd100f8c3f0
Oscar Lodriguez <oscarl@backbase.com>
2016-07-19 16:16:13 +0200

    templates

--- main.py
+++ main.py
@@ -14,12 +14,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+import jinja2
+from os import path
+
+template_dir = path.join(path.dirname(__file__), 'partials')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(Handler):
     def get(self):
-        self.response.write('Hello !')
+        self.render("shopping_list.html", poop=self.request.get("name"))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
